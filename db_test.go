884bafaedd04e579a286371325e709d1509c71bf
Eric Schmidt <erschmid@google.com>
2024-10-16 18:53:21 +0000

    chore: added test for get conversations

--- db_test.go
+++ db_test.go
@@ -1,30 +1,63 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os"
 	"testing"
 	"time"
+
+	"cloud.google.com/go/firestore"
 )
 
 var (
-	email      string = "unittest@example.com"
-	_projectID string = "erschmid-test-291318"
+	email           string = "unittest@example.com"
+	email2          string = "unittest2@example.com"
+	_projectID      string
+	_collectionName string = "TestCollection"
 )
 
 func TestMain(m *testing.M) {
+	_projectID = os.Getenv("PROJECT_ID")
+	ctx := context.Background()
+	client, err := firestore.NewClientWithDatabase(ctx, _projectID, DBName)
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	defer client.Close()
+
+	collection := client.Collection(_collectionName)
+	subcollection := collection.Doc(email2).Collection(SubCollectionName)
+	_, _, err = subcollection.Add(ctx, ConversationBit{
+		UserQuery:   "test user query",
+		BotResponse: "test bot response",
+		Created:     time.Now(),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, _, err = subcollection.Add(ctx, ConversationBit{
+		UserQuery:   "test user query 2",
+		BotResponse: "test bot response 2",
+		Created:     time.Now(),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	m.Run()
 
-	// Clean up after tests run
-	// ctx := context.Background()
-	// client, err := firestore.NewClientWithDatabase(ctx, _projectID, DBName)
-	// if err != nil {
-	// 	log.Fatal(err)
+	//Clean up after tests run
+	_, err = collection.Doc(email).Delete(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// }
-	// collection := client.Collection(CollectionName)
-	// _, err = collection.Doc(email).Delete(ctx)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	_, err = collection.Doc(email2).Delete(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func TestSaveConversation(t *testing.T) {
@@ -46,5 +79,17 @@ func TestSaveConversation(t *testing.T) {
 	err = saveConversation(*nextConvo, email, _projectID)
 	if err != nil {
 		t.Fatalf("Error on adding next conversation: %v\n\n", err)
+	}
+}
+
+func TestGetConversation(t *testing.T) {
+	conversations, err := getConversation(email2, projectID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := 2
+	if got := len(conversations); got != want {
+		t.Errorf("wanted %d messages, got %d", want, got)
 	}
 }
